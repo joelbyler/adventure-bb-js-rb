b87dfc7e9d2c04af05f6e86eaa01dd8922a8c476
Joel Byler <joelbyler@gmail.com>
2013-10-19 20:00:24 -0400

    add template for questions

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 $(function() {
   App.currentUser = new App.Models.User();
   var loginView = new App.Views.Login({el: $('#loginView'), model: App.currentUser});
-  var newQuestionView = new App.Views.NewQuestion({el: $('#newQuestionView')});
+
+  var questions = new App.Collections.Questions();
+  var newQuestionView = new App.Views.NewQuestion({el: $('#newQuestionView'), model: questions});
+  var questionsView = new App.Views.Questions({el: $('#questions'), model: questions});
 });
--- scripts/views/new_question.js
+++ scripts/views/new_question.js
@@ -1,14 +1,22 @@
 App.Views.NewQuestion = Backbone.View.extend({
- 
+  events: {
+    "click #addItem": "addItem"
+  }, 
   initialize: function(options) {
     App.currentUser.on("change", this.render, this);
     this.render();
   },
- 
   render: function() {
     this.$("#asker").text(App.currentUser.get("userName"));
     this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
     return this;
-  }
- 
+  },
+  addItem: function() {
+    var question = {
+      text: this.$("#newQuestion").val(),
+      userName: App.currentUser.get("userName")
+    };
+    this.model.add(new App.Models.Question(question));
+    this.$("#newQuestion").val('');
+  } 
 });
