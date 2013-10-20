436fe222f95b561ed093c9b3c0f564e857c338c2
Joel Byler <joelbyler@gmail.com>
2013-10-19 21:03:46 -0400

    Add rails backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdventureBbJsRb::Application.config.secret_key_base = '948b5a25b3117b02eb0506eb96da2516c8bedec4daa2c802f3a5d18c41adb415cc2782b95bf61a472ca2c20d2d3ce0bf09a502ac8d52141cd9ecc14c11eafb37'
